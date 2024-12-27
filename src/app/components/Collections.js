53146fa366d18b02ab10edfb41c6ad617f4d4d28
Loris DUCAMPS <lducamps@corp.free.fr>
2024-12-27 10:40:22 +0100

    update collections

--- src/app/components/Collections.js
+++ src/app/components/Collections.js
@@ -37,34 +37,39 @@ const collectionsData = [
 
 const Collections = () => {
   return (
-    <section className="my-[120px]" aria-labelledby="collection-heading">
-      <h2 className="font-fraunces font-black text-[40px] leading-[72px] bg-text-gradient bg-clip-text text-fill-transparent text-center mb-[13px]">
-        our collection
-      </h2>
-      <div className="grid grid-cols-1 px-6 gap-12 desktop:grid-cols-4">
-        {collectionsData.map((item) => (
-          <article key={item.id} className="tablet:flex desktop:block">
-            <figure>
-              <Image
-                src={item.src}
-                alt={item.alt}
-                width={256}
-                height={194}
-                loading="lazy"
-                className="mx-auto max-w-[200px]"
-              />
-              <figcaption className="sr-only">{item.alt}</figcaption>
-            </figure>
-            <div>
-              <h3 className="font-fraunces font-black text-[24px] leading-[32px] text-center mt-6 mb-4">
-                {item.title}
-              </h3>
-              <p className="text-[15px] leading-[25px] text-center">
-                {item.description}
-              </p>
-            </div>
-          </article>
-        ))}
+    <section
+      className="my-[120px] tablet:my-[144px] desktop:mt-[72px] desktop:mb-[200px]"
+      aria-labelledby="collection-heading"
+    >
+      <div className="desktop:max-w-[1111px] desktop:mx-auto">
+        <h2 className="relative font-fraunces font-black text-[40px] leading-[72px] bg-text-gradient bg-clip-text text-fill-transparent text-center mb-[13px] -z-[1] tablet:text-[96px] tablet:leading-[72px] tablet:-bottom-10 tablet:mb-0 desktop:text-[150px] desktop:leading-none desktop:-bottom-16">
+          our collection
+        </h2>
+        <div className="grid grid-cols-1 px-6 gap-12 tablet:gap-8 desktop:grid-cols-4 desktop:px-0">
+          {collectionsData.map((item) => (
+            <article key={item.id} className="tablet:flex desktop:block">
+              <figure>
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  width={256}
+                  height={194}
+                  loading="lazy"
+                  className="mx-auto max-w-[200px] tablet:max-w-none"
+                />
+                <figcaption className="sr-only">{item.alt}</figcaption>
+              </figure>
+              <div>
+                <h3 className="font-fraunces font-black text-[24px] leading-[32px] text-center mt-6 mb-4 tablet:text-left desktop:text-center">
+                  {item.title}
+                </h3>
+                <p className="text-[15px] leading-[25px] text-center tablet:text-left desktop:text-[16px] desktop:leading-[26px] desktop:text-center">
+                  {item.description}
+                </p>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,53 +1,50 @@
 import localFont from 'next/font/local';
-import "./globals.css";
-import React from "react";
-import Header from './components/Header';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import './globals.css';
 
 export const metadata = {
-    title: "CoffeeRoasters",
-    description: "Une description de mon application",
+  title: 'CoffeeRoasters',
+  description: 'Une description de mon application',
 };
 
 const barlow = localFont({
-    src: [
-        {
-            path: './fonts/barlow-regular.woff2',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: './fonts/barlow-bold.woff2',
-            weight: '700',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-barlow',
+  src: [
+    {
+      path: './fonts/barlow-regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/barlow-bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-barlow',
 });
 
 const fraunces = localFont({
-    src: [
-        {
-            path: './fonts/fraunces-black.woff2',
-            weight: '900',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-fraunces',
+  src: [
+    {
+      path: './fonts/fraunces-black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-fraunces',
 });
 
-export default function RootLayout({children}) {
-    return (
-        <html lang="fr" className={`${barlow.variable} ${fraunces.variable}`}>
-        <body className="antialiased font-barlow">
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr" className={`${barlow.variable} ${fraunces.variable}`}>
+      <body className="antialiased font-barlow">
         <div className="max-w-[1360px] mx-auto px-6 tablet:px-10">
-            <Header />
-            <main>
-                {children}
-            </main>
-            <Footer/>
+          <Header />
+          <main>{children}</main>
+          <Footer />
         </div>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
