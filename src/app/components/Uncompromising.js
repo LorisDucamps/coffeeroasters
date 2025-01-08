4f842edd8e82309847249c6bfb9091748415b482
Loris DUCAMPS <lducamps@corp.free.fr>
2025-01-08 10:35:20 +0100

    update

--- src/app/components/Uncompromising.js
+++ src/app/components/Uncompromising.js
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 
 const beforeStyles =
-  "before:content-[''] before:bg-darked-grey-blue before:absolute before:top-[25%] before:left-0 before:right-0 before:h-[509px] before:rounded-[10px] before:z-[-1] before:tablet:h-[488px] before:desktop:h-[474px]";
+  "before:content-[''] before:bg-darked-grey-blue before:absolute before:top-[25%] before:left-0 before:right-0 before:h-[509px] before:rounded-[10px] before:z-[-1] before:tablet:h-[488px] before:desktop:h-[474px] before:desktop:top-[88px]";
 
 const Uncompromising = () => {
   return (
     <section className={`relative ${beforeStyles} mb-[120px]`}>
-      <div className="grid grid-cols-1 desktop:grid-cols-2 gap-16">
-        <picture className="relative w-full px-6">
+      <div className="grid grid-cols-1 desktop:grid-cols-2 gap-16 desktop:max-w-[1110px] desktop:mx-auto">
+        <picture className="relative w-full px-6 tablet:px-14 desktop:order-1">
           <source
             srcSet="/uncompromising-mobile.jpg 1x, /uncompromising-mobile@2x.jpg 2x"
             media="(max-width: 599px)"
@@ -29,11 +29,11 @@ const Uncompromising = () => {
             priority
           />
         </picture>
-        <div className="px-6">
-          <h3 className="font-fraunces font-black text-[28px] leading-[28px] text-center text-light-cream mb-6 tablet:text-left desktop:text-[40px] desktop:leading-[48px] desktop:text-left">
+        <div className="px-6 tablet:px-16">
+          <h3 className="font-fraunces font-black text-[28px] leading-[28px] text-center text-light-cream mb-6 tablet:text-center desktop:text-[40px] desktop:leading-[48px] desktop:text-left">
             Uncompromising quality
           </h3>
-          <p className="text-[15px] leading-[25px] text-light-cream text-center tablet:text-left desktop:text-left">
+          <p className="text-[15px] leading-[25px] text-light-cream text-center tablet:text-center desktop:text-left">
             Although we work with growers who pay close attention to all stages
             of harvest and processing, we employ, on our end, a rigorous quality
             control program to avoid over-roasting or baking the coffee dry.
